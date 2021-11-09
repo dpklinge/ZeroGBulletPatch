6652a80f8c48332842bea10e6b6d0aaae094e34a
Daniel Klingensmith <dpklinge@ncsu.edu>
2021-11-09 15:33:25 -0500

    Intial commit

---
+++ ZeroGBulletPatch/ZeroGBulletPatch.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace ZeroGBulletPatch
+{
+    
+    [BepInPlugin(ModId, ModName, "1.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class ZeroGBulletPatch : BaseUnityPlugin
+    {
+        private const string ModId = "com.dk.rounds.plugins.zerogpatch";
+        private const string ModName = "ZeroGBulletPatch";
+
+        private void Awake()
+        {
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        private void Start()
+        {
+        }
+    }
+
+    [Serializable]
+    [HarmonyPatch(typeof(GeneralInput), "Update")]
+    class GeneralInputZeroGPatch
+    {
+        // remove speed compensation (makes zero-g bullets actually shoot straight)
+        private static void Postfix(GeneralInput __instance)
+        {
+            if (((CharacterData)Traverse.Create(__instance).Field("data").GetValue()).weaponHandler.gun.gravity == 0)
+            {
+                __instance.aimDirection -= Vector3.up * 0.13f / Mathf.Clamp(((CharacterData)Traverse.Create(__instance).Field("data").GetValue()).weaponHandler.gun.projectileSpeed, 1f, 100f);
+            }
+        }
+    }
+}
